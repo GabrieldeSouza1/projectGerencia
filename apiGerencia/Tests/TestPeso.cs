49342c383ee929427eda18c1e8a96d2025b64254
GabrieldeSouza1 <gabrieldesouza125@gmail.com>
2024-05-30 15:17:37 -0300

    (CI) Mudança para testar a pipe

--- apiGerencia/Tests/TestPeso.cs
+++ apiGerencia/Tests/TestPeso.cs
@@ -10,7 +10,7 @@ namespace apiGerencia.Tests
         [TestMethod]
         public void testObterPesoIdeal()
         {
-            string teste = "Apenas par haver mudaça no codigo v3";
+            string change = "change";
             var metodos = new Metodos();
             double result = metodos.obterPesoIdeal(1.80, 'M') ;
 
