314032c5ada669fd583798c657f2efd44d61014f
GabrieldeSouza1 <gabrieldesouza125@gmail.com>
2024-05-30 15:25:10 -0300

    (CI) Mudança

--- apiGerencia/Tests/TestPeso.cs
+++ apiGerencia/Tests/TestPeso.cs
@@ -10,7 +10,7 @@ namespace apiGerencia.Tests
         [TestMethod]
         public void testObterPesoIdeal()
         {
-            string change = "changes";
+            string change = "change";
             var metodos = new Metodos();
             double result = metodos.obterPesoIdeal(1.80, 'M') ;
 
